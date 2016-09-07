a425fc6d0a5d5d98770ca105c25201cc2b1aad86
gollamudis7042 <GollamudiS7042@sduhsd.lan>
2016-09-07 13:31:07 -0700

    Added Lab 2 and notes

---
+++ AP Comp Sci Lesson 2/lab2.java
@@ -0,0 +1,19 @@
+public class lab2
+{
+	public static void main(String[]args)
+	{
+		int x = 1;
+		int y = 2;
+		String Name = "Sagar";
+		String address = "Willowmere Lane";
+		String city = "San Diego";
+		String zip = "92130";
+		int length = 3;
+		int width = 3;
+		int height = 3;
+		System.out.println("The surface area of a cube with edge length 3 is " + length*width*6);
+		System.out.println("For " + Name + " The address is " + address + " , " + city + " , " + zip);
+		System.out.println("The value of 1 times 2 is " + x * y);
+	
+	}
+}
