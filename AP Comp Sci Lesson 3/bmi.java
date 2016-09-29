99a394bded32a3dcbb4eb99d04cfca80653d9ee2
gollamudis7042 <GollamudiS7042@sduhsd.lan>
2016-09-29 14:19:51 -0700

    split prompts for weight and height

--- AP Comp Sci Lesson 3/bmi.java
+++ AP Comp Sci Lesson 3/bmi.java
@@ -5,8 +5,9 @@ public class bmi
 	public static void main(String[]args)
 	{
 		Scanner keyboard = new Scanner(System.in);
-		System.out.println("Please enter your weight in pounds and height in inches");
+		System.out.println("Please enter your weight in pounds");
 		double num = keyboard.nextDouble();
+		System.out.println("Please enter your height in inches");
 		double num2 = keyboard.nextDouble();
 		double num3 = num/(num2*num2);
 		System.out.println("Your BMI is " + num3 * 703 + ".");
