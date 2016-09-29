f0309a51d0c0a542dedb56baf1ee411126d3e996
gollamudis7042 <GollamudiS7042@sduhsd.lan>
2016-09-29 13:01:03 -0700

    fixed the height and weight prompts

--- AP Comp Sci Lesson 3/Calculation.java
+++ AP Comp Sci Lesson 3/Calculation.java
@@ -7,6 +7,8 @@ public class Calculation
 		Scanner keyboard = new Scanner(System.in);
 		System.out.println("Give me a length");
 		int num = keyboard.nextInt();
-		System.out.println("A square with this side length has an area of " + num * num + ".");
+		System.out.println("Give me a height");
+		int num1 = keyboard.nextInt();
+		System.out.println("A rectangle with length " + num + " and height " + num1 + " has an area of " + num * num1 + ".");
 	}
 }
