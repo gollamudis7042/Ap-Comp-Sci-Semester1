5c2777e74b03f28ad96a11663c44b66c7e18e530
gollamudis7042 <GollamudiS7042@sduhsd.lan>
2016-10-07 13:03:32 -0700

    fixed formatting

--- AP Comp Sci Lesson 2/lab2.java
+++ AP Comp Sci Lesson 2/lab2.java
@@ -2,15 +2,16 @@ public class lab2 {
 	public static void main(String[]args) {
 		int x = 1;
 		int y = 2;
-		String Name = "Sagar";
+		String Name = "Sagar Gollamudi";
 		String address = "Willowmere Lane";
 		String city = "San Diego";
+		String state = "CA";
 		String zip = "92130";
 		int length = 3;
 		int width = 3;
 		int height = 3;
 		System.out.println("The surface area of a cube with edge length 3 is " + length*width*6);
-		System.out.println("For " + Name + " The address is " + address + " , " + city + " , " + zip);
+		System.out.println(Name + "\n" + address + "\n" + city + ", " + state + " " + zip);
 		System.out.println("The value of 1 times 2 is " + x * y);
 	
 	}
