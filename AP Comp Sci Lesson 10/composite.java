5d79ef485325792c4c47b8bb36623e9015f4d87a
gollamudis7042 <GollamudiS7042@sduhsd.lan>
2016-11-28 14:27:38 -0800

    lesson 10

---
+++ AP Comp Sci Lesson 10/composite.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class composite
+{	
+	public static void main(String[]args)
+	{
+		Integer[] num = {2,6,8,9,10,12,13,15,17,24,55,66,78,77,79};
+		ArrayList<Integer> n = new ArrayList<> (Arrays.asList(num));
+		
+		for(int i=0;n.get(i)<=n.size();i++)
+		{
+			if(gF(n.get(i))==0)
+				n.remove(i);
+		}
+		System.out.println(n);
+	}
+	public static Integer gF(int n)
+	{
+		for(int i=2;i<n;i++)
+		{
+			if(n%i==0)
+				return 1;
+			else 
+				return 0;
+		}
+	}
+}
